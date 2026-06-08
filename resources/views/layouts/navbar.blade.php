d97bf316578b9cc2bbf4a196189a66876190e3ae
Yrbre <bariqfajar60@gmail.com>
2026-06-08 16:28:51 +0700

    View: New logo For Navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,14 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white flex-row border-bottom shadow">
     <div class="container-fluid">
         <a class="navbar-brand mx-lg-1 mr-0" href="{{ route('dashboard') }}">
-            <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve">
-                <g>
-                    <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
-                    <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
-                    <polygon class="st0" points="78,33 15,33 24,15 87,15 	" />
-                </g>
-            </svg>
+            <img src="{{ asset('design/dark/assets/images/LogoTifico.png') }}" alt="Logo" style="width: 10vh;">
         </a>
         <button class="navbar-toggler mt-2 mr-auto toggle-sidebar text-muted">
             <i class="fe fe-menu navbar-toggler-icon"></i>
